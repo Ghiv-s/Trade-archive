00c002a22e1acbfb44d73f048372106dd46a4d4f
Ghiv-s <ilie.gherman.v@mail.com>
2026-03-02 17:16:13 +0200

    Switch to Waitress for Render deployment

--- app.py
+++ app.py
@@ -134,6 +134,9 @@ def trades():
 
 import os
 
+from waitress import serve
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    serve(app, host="0.0.0.0", port=port)
